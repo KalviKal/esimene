cab767762e6e5fa4c95d07f010977609ccc073f0
KalviKal <66356112+KalviKal@users.noreply.github.com>
2025-04-23 19:19:43 +0300

    Initial commit

---
+++ src/Luuletus.jsx
@@ -0,0 +1,14 @@
+function Luuletus() {
+    return (
+      <>
+        <p>Kes hommikuti külas käib,</p>
+        <p>see asjatult ei longi</p>
+        <p>sest selleks hommik ongi</p>
+        <div className="eriline">
+            <p>sest selleks hommik ongi</p>
+        </div>
+      </>
+    )
+}
+
+export default Luuletus
